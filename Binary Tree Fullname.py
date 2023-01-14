aa66d29b9da3b92269d9ca40785cdc7bea739c20
aengraciaaguilar <aengraciaaguilar@gmail.com>
2023-01-14 18:37:55 +0800

    first binary class

---
+++ Binary Tree Fullname.py
@@ -0,0 +1,9 @@
+# Laboratory Activity # 6
+# Name: Aengracia Aguilar
+# Course and Year: BSCOE 2-2
+
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
--- main.py
+++
@@ -1,3 +0,0 @@
-# Laboratory Activity # 6
-# Name: Aengracia Aguilar
-# Course and Year: BSCOE 2-2
