1f8c1ae7d77def6bed6e673606ae4d724782f85c
aengraciaaguilar <aengraciaaguilar@gmail.com>
2023-01-14 18:45:45 +0800

    sixth def function called find_max

--- Binary Tree Fullname.py
+++ Binary Tree Fullname.py
@@ -70,3 +70,8 @@ class BinarySearchTreeNode:
             elements += self.right.pre_order_traversal()
 
         return elements
+
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.find_max()
