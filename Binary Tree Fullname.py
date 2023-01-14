00ed6e080d5da2f06d35a41a3e45cf7af19d524f
aengraciaaguilar <aengraciaaguilar@gmail.com>
2023-01-14 19:56:48 +0800

    printing the calculate_sum function

--- Binary Tree Fullname.py
+++ Binary Tree Fullname.py
@@ -151,3 +151,4 @@ if __name__ == '__main__':
     phonenumber = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     ph_tree = build_tree(phonenumber)
     print("My phonenumber:", phonenumber)
+    print("Sum:", ph_tree.calculate_sum())
